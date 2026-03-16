a2a5c0db8335acab967b135e89198501aa718a32
Mantas Lazauskas <mantas.lazauskas@mail.bcu.ac.uk>
2026-03-16 16:40:00 +0000

    Completed lab 8

--- Labs/week8/Shading.hpp
+++ Labs/week8/Shading.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <Eigen/Dense>
+#include <cmath>
+#include <algorithm>
 
 // Subtask 1: Implement the Reflect function in Shading.hpp, so you can find the reflected vector.
 /// <summary>
@@ -10,10 +12,8 @@
 /// <returns>Reflected vector, pointing out from surface point.</returns>
 Eigen::Vector3f reflect(const Eigen::Vector3f& incoming, const Eigen::Vector3f& normal)
 {
-	// *** YOUR CODE HERE ***
-	// replace this with the reflected vector.
-	return Eigen::Vector3f::Zero();
-	// *** END YOUR CODE ***
+	// Reflection formula: r = incoming - 2 * (incoming . normal) * normal
+	return incoming - 2.0f * incoming.dot(normal) * normal;
 }
 
 // Subtask 2: Implement the Phong specular term in Shading.hpp, to find the intensity of specular reflection
@@ -28,19 +28,17 @@ Eigen::Vector3f reflect(const Eigen::Vector3f& incoming, const Eigen::Vector3f& 
 /// <returns>Specular term (number from 0 to 1)</returns>
 float phongSpecularTerm(const Eigen::Vector3f& incomingLightDir, const Eigen::Vector3f& normal, const Eigen::Vector3f& viewDir, float exponent)
 {
-	// *** YOUR CODE HERE ***
 	// Find the reflected direction using the reflect function
-	Eigen::Vector3f reflectionDir = Eigen::Vector3f::Zero();
+	Eigen::Vector3f reflectionDir = reflect(incomingLightDir, normal).normalized();
 
 	// Find dot product between reflected and view directions.
-	float reflectDotNorm = 0.f;
+	float reflectDotView = reflectionDir.dot(viewDir);
 
 	// Make sure dot product is non-negative (if it's less than 0, set it to 0!)
-	reflectDotNorm = 0.f;
+	reflectDotView = std::max(reflectDotView, 0.0f);
 
 	// Finally, raise to specular exponent and return.
-	return 0.f;
-	// *** END YOUR CODE ***
+	return powf(reflectDotView, exponent);
 }
 
 // Subtask 5: Implement the Blinn-Phong reflection model in Shading.hpp
@@ -55,17 +53,17 @@ float phongSpecularTerm(const Eigen::Vector3f& incomingLightDir, const Eigen::Ve
 /// <returns>Specular term (number from 0 to 1)</returns>
 float blinnPhongSpecularTerm(const Eigen::Vector3f& incomingLightDir, const Eigen::Vector3f& normal, const Eigen::Vector3f& viewDir, float exponent)
 {
-	// *** YOUR CODE HERE ***
 	// Find the half-vector (average of view dir and light dir)
-	Eigen::Vector3f halfVec = Eigen::Vector3f::Zero();
+	// Note: incomingLightDir is from light towards surface, so the vector from surface to light is -incomingLightDir.
+	Eigen::Vector3f halfVec = (viewDir + (-incomingLightDir)).normalized();
 
 	// Find dot product of half-vector and normal.
-	float halfDotNorm = 0.f;
-	
+	float halfDotNorm = halfVec.dot(normal);
+
 	// Force the dot product to be non-negative (if <0, set to 0)
+	halfDotNorm = std::max(halfDotNorm, 0.0f);
 
 	//Return the dot product raised to the exponent
-	return 0.f;
-	// *** END YOUR CODE ***
+	return powf(halfDotNorm, exponent);
 }
 
